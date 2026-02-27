d585d722a09301b81bc6687028280e8240f15613
gota125 <issanzeza@gmail.com>
2026-02-27 23:44:16 +0100

    V0.28 porte animation

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -1,14 +1,38 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class ExitDoor : MonoBehaviour
 {
+    private Animator anim;
+    private bool aEteTouchee = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         // On vérifie si c'est le joueur qui touche la porte
         if (collision.CompareTag("Player"))
         {
+            aEteTouchee = true;
+            StartCoroutine(SeqenceSortie());
             Debug.Log("Niveau terminé ! Passage au suivant...");
-            GameManager.NextLevel();
+           
         }
     }
-}
+
+    IEnumerator SeqenceSortie()
+    {
+        if (anim != null)
+        {
+            anim.SetTrigger("FERMETURE");
+        }
+        yield return new WaitForSeconds(0.8f);
+             GameManager.NextLevel();
+        }
+    }
+
