b889fc77464525d8f4357551a227d1276037927b
gota125 <issanzeza@gmail.com>
2026-02-27 03:03:48 +0100

    V0.22 Vfx fireball

--- Assets/Scripts/Projectile (2).cs
+++ Assets/Scripts/Projectile (2).cs
@@ -1,9 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     public float lifeTime = 3f;
-
+    public GameObject impactVFX;
     void Start()
     {
         Destroy(gameObject, lifeTime);
@@ -17,17 +18,27 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("MurDestructble"))
-        {
+        { if(impactVFX!=null);
+                     {
+                         GameObject effect = Instantiate(impactVFX, transform.position, Quaternion.identity);
+                     }
             Destroy(gameObject);
             Destroy(collision.gameObject);
+           
         } if (collision.CompareTag("Ground"))
-        {
+        {if(impactVFX!=null);
+                              {
+                                  GameObject effect = Instantiate(impactVFX, transform.position, Quaternion.identity);
+                              }
             Destroy(gameObject);
             Debug.Log(" toucher le mur");
         }
 
         if (collision.CompareTag("Danger"))
-        {
+        {if(impactVFX!=null);
+            {
+                GameObject effect = Instantiate(impactVFX, transform.position, Quaternion.identity);
+            }
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
